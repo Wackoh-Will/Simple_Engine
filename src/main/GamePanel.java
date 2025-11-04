3767cc7dfdf3460c0cd52948a56b36fc44c2b0dc
Nathan <nathan.will.tyler@gmail.com>
2025-11-04 12:42:21 -0500

    Made an enemy no code yet
    Just added the file

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -67,18 +67,14 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
 
-        if (code == KeyEvent.VK_W) {
-            upPressed = true;
-        }
-        if (code == KeyEvent.VK_S) {
-            downPressed = true;
-        }
-        if (code == KeyEvent.VK_A) {
-            leftPressed = true;
-        }
-        if (code == KeyEvent.VK_D) {
-            rightPressed = true;
-        }
+        if (code == KeyEvent.VK_W) upPressed = true;
+
+        if (code == KeyEvent.VK_S) downPressed = true;
+
+        if (code == KeyEvent.VK_A) leftPressed = true;
+
+        if (code == KeyEvent.VK_D) rightPressed = true;
+
         if (code == KeyEvent.VK_E) ePressed = false;
     }
 
@@ -86,18 +82,14 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
 
-        if (code == KeyEvent.VK_W) {
-            upPressed = false;
-        }
-        if (code == KeyEvent.VK_S) {
-            downPressed = false;
-        }
-        if (code == KeyEvent.VK_A) {
-            leftPressed = false;
-        }
-        if (code == KeyEvent.VK_D) {
-            rightPressed = false;
-        }
+        if (code == KeyEvent.VK_W) upPressed = false;
+
+        if (code == KeyEvent.VK_S) downPressed = false;
+
+        if (code == KeyEvent.VK_A) leftPressed = false;
+
+        if (code == KeyEvent.VK_D) rightPressed = false;
+
         if (code == KeyEvent.VK_E) ePressed = true;
     }
 
