67712114f1d586f9b2f92ec81a9a2c61b761f674
Nathan <nathan.will.tyler@gmail.com>
2025-12-06 00:16:23 -0500

    added more to the Guns
    draws the gun and has it rotate with the mouse position

--- src/Weapons/Gun.java
+++ src/Weapons/Gun.java
@@ -1,5 +1,9 @@
 package Weapons;
+import entity.Player;
 import main.GamePanel;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public abstract class Gun {
     protected int ammo;
@@ -8,16 +12,40 @@ public abstract class Gun {
     protected int reload_time;
     protected int fire_time;
 
+    protected BufferedImage sprite;
+
     public Gun(int magSize, int damage, int reload_time, int fire_time) {
         this.magSize = magSize;
         this.damage = damage;
         this.ammo = magSize;
+        loadSprite();
     }
     public abstract void reload(GamePanel gp);
     public abstract void shoot(GamePanel gp);
     public abstract void r_count(GamePanel gp);
 
+    protected abstract void loadSprite();
+
+    public void draw(Graphics g, int x, int y, int mouseX, int mouseY ) {
+        if (sprite == null) return;
+
+        Graphics2D g2 = (Graphics2D) g.create();
+
+        double angle = Math.atan2(mouseY - y, mouseX - x);
+
+        int centerX = sprite.getWidth() / 2;
+        int centerY = sprite.getHeight() / 2;
+        g2.rotate(angle, x + centerX, y + centerY);
+
+        g2.drawImage(sprite, x, y, null);
+
+        g2.dispose();
+    }
+
+
     public void update(GamePanel gp) {
-        //Still need to finish this
+        reload(gp);
+        shoot(gp);
+        r_count(gp);
     }
 }
